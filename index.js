490a49a7e3b65bf0e0e056c513a8e371ad51ef56
user <mail>
2015-05-03 23:34:02 +0300

    bot now replies to messages :)

--- index.js
+++ index.js
@@ -2,46 +2,54 @@
  * Created by lsd on 27/04/15.
  */
 var OKCupid = require('okcupidjs');
-var okcMethods = require('./lib/okcMethods');
+var Q = require('q');
 
-var okc = new OKCupid();
+var girl = new OKCupid();
 
-var storage = [
-    {
-        receiverId: '18257544100032278490',
-        receiverName: 'diethylamide',
-        messagesHandeled: []
-    }
-];
+console.log("Welcome..");
+girl.login('coralasaurustron', 'OKq45y0P')
+    .then(function() {
+        console.log("logged in");
+        girl.loadAccessToken().then(function() {
+            console.log("Loaded access token");
+            forwardNewMessages(girl);
+        }, function(error) {
+            console.log("couldn't get conversations, error: " + error );
+        });
+    }, function(error) {
+        console.log("couldn't login, error: " + error);
+    });
 
+function forwardNewMessages(cupidAccount) {
+    var conversation = "9945336917231285831";
+    cupidAccount.getConversation(conversation)
+        .then(function(data) {
+            if (data.body && data.body.messages && data.body.messages.length > 0) {
+                console.log("Got messages");
 
-okc.login('coralasaurustron', 'OKq45y0P', function(error, response) {
-    if(response.headers.location && response.headers.location === '/home') {
-   /*     okc.getConversations(function(conversations) {
-            var unreadConversations = okcMethods.findUnreadMessages(conversations);
-            console.log("all messages: " + conversations.length);
-            console.log("unread messages: " + unreadConversations.length);
-            okc.getConversation(unreadConversations[0].thread_id, function(error, response, body) {
-                console.log(body);
-            })
-        })*/
+                cupidAccount.deleteConversation(conversation)
+                    .then(function () {
+                        console.log("and deleted the conversation");
+                        var sendingMessagePromises = [];
+                        data.body.messages.reverse().forEach(function (message) {
+                            if(!message.from_me) {
+                                sendingMessagePromises.push(cupidAccount.sendMessage(message.senderid, conversation, message.body));
+                            }
+                        });
 
-        getNewMessagesForConversation("9945336917231285831");
+                        Q.all(sendingMessagePromises)
+                            .then(function() {
+                                setTimeout(forwardNewMessages.bind(this, cupidAccount), 5000);
+                            });
 
+                    });
 
-       /* okc.sendMessage("18257544100032278490", "9945336917231285831", "testing", "1,0,1430496932,0x775b3c9b9255d99d;1c9e10861b19763f62bc55ed63f94d23fe57110f", function(error, response, body) {
-            if(error) console.log("error: " + error);
-            else console.dir(body);
-        })*/
-    }
-    else {
-        console.log("Error logging in.");
-    }
-});
-
-
-function getNewMessagesForConversation(conversationId, callback) {
-    okc.getConversation(conversationId, function(error, response, body) {
-        console.log(body);
-    })
+            }
+            else {
+                console.log("no new messages");
+                setTimeout(forwardNewMessages.bind(this, cupidAccount), 5000);
+            }
+        }, function(error) {
+            console.log("couldn't get conversation, error: " + error)
+        });
 }
